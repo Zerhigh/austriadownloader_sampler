dcaf2896d691a2bd2d1632f35ff7fef18e65f64d
Zerhigh <samuel@plix.at>
2025-02-27 16:14:24 +0100

    loading and transforming corine

--- Sampler.py
+++ Sampler.py
@@ -1,21 +1,32 @@
 import os.path
 
-import rasterio
-import numpy as np
 import geopandas as gpd
+import numpy as np
+import rasterio
 import shapely
 from rasterio.mask import mask
+from rasterio.warp import calculate_default_transform, reproject, Resampling
+from shapely.geometry import Point
+
+
+class Config:
+    def __init__(self, pixel_size=1.6, shape=(4, 1024, 1024)):
+        self.pixel_size = pixel_size
+        self.shape = shape
 
 
 class Sampler:
-    def __init__(self, sample_path, outpath_cropped_corine, corine_path, verbose=False):
+    def __init__(self, sample_path, outpath_cropped_corine, corine_path, config, verbose=False):
         """
         Initialize the RasterReader with a given file path.
         """
         self.sample_path = sample_path
         self.corine_path = corine_path
         self.outpath_cropped_corine = outpath_cropped_corine
+        self.config = config
         self.verbose = verbose
+
+        self.lambert = 'EPSG:31287'
         self.corine = None
         self.corine_landcover = {111: "Continuous urban fabric",
                                  112: "Discontinuous urban fabric",
@@ -64,31 +75,45 @@ class Sampler:
 
     @property
     def border_austria(self):
-        fp_austria = r'C:\Users\PC\Coding\GeoQuery\austriadownloader\austria_data\matched_metadata.gpkg'
+        fp_austria = 'data/matched_metadata.gpkg'
         fp = gpd.read_file(fp_austria)
         border = shapely.union_all(fp.geometry)
         return gpd.GeoDataFrame(geometry=[border], crs=fp.crs)
 
-    def load_corine(self):
-        """
-        Open the raster file using rasterio.
-        """
+    def transform_corine(self, src, outpath):
         try:
-            if os.path.exists(self.outpath_cropped_corine):
-                if self.verbose:
-                    print(f'loading corine from: {self.outpath_cropped_corine}')
-                self.corine = rasterio.open(self.outpath_cropped_corine)
-            else:
-                if self.verbose:
-                    print('creating corine cropped image')
-                self.crop_full_corine()
-                self.corine = rasterio.open(self.outpath_cropped_corine)
+            # Compute new transform and dimensions
+            transform, width, height = calculate_default_transform(
+                src.crs, self.lambert, src.width, src.height, *src.bounds
+            )
+
+            # Define metadata for the output raster
+            new_meta = src.meta.copy()
+            new_meta.update({
+                "crs": self.lambert,
+                "transform": transform,
+                "width": width,
+                "height": height
+            })
+
+            # Open output raster and reproject
+            with rasterio.open(outpath, "w", **new_meta) as dst:
+                reproject(
+                    source=src.read(1),  # Read single band
+                    destination=rasterio.band(dst, 1),
+                    src_transform=src.transform,
+                    src_crs=src.crs,
+                    dst_transform=transform,
+                    dst_crs=self.lambert,
+                    resampling=Resampling.nearest  # Change to bilinear/cubic if needed
+                )
+            return outpath
         except Exception as e:
-            print(f"Error opening corine: {e}")
+            print(f"Error transforming raster to {self.lambert}: {e}")
 
     def crop(self, src, gdf):
         try:
-            # Transform the geometry to match the raster's CRS
+            # Transform the geometry to match the raster's CRS - not necessary as i transform the raster
             gdf = gdf.to_crs(src.crs)
 
             # Clip the raster using the geometry
@@ -106,49 +131,82 @@ class Sampler:
         except Exception as e:
             print(f"Error clipping raster: {e}")
 
-    def crop_full_corine(self):
-        if os.path.exists(self.outpath_cropped_corine):
-            print('cropped file already exists')
-        else:
-            try:
-                # Open the raster file
-                with rasterio.open(self.corine_path) as src:
-                    out_image, out_meta = self.crop(src=src, gdf=self.border_austria)
+    def process_corine(self):
+        # croP first, then trasnform
+        cropped_corine = 'data/corine/cropped.tif'
 
-                    # Save the clipped raster
-                    with rasterio.open(self.outpath_cropped_corine, "w", **out_meta) as dest:
-                        dest.write(out_image)
+        # Open the raster file
+        with rasterio.open(self.corine_path) as src:
+            out_image, out_meta = self.crop(src=src, gdf=self.border_austria)
 
-                if self.verbose:
-                    print(f"Clipped raster saved to: {self.outpath_cropped_corine}")
+            # Save the clipped raster
+            with rasterio.open(cropped_corine, "w", **out_meta) as dest:
+                dest.write(out_image)
 
-            except Exception as e:
-                print(f"Error clipping full raster: {e}")
+        # crop with austrian bordders extent
+        with rasterio.open(cropped_corine) as src:
+            # save as file
+            self.transform_corine(src=src, outpath=self.outpath_cropped_corine)
 
         return
 
-    def get_corine_value(self, x, y):
+    def load_corine(self):
         """
-        Get the pixel value at the given (x, y) coordinate.
-        :param x: X coordinate in pixel space
-        :param y: Y coordinate in pixel space
-        :return: Pixel value(s) at the given location
+        Open the raster file using rasterio.
         """
-        if self.corine:
-            return self.corine.read(window=((y, y + 1), (x, x + 1))).squeeze()
+        if os.path.exists(self.outpath_cropped_corine):
+            if self.verbose:
+                print(f'loading corine from: {self.outpath_cropped_corine}')
+            self.corine = rasterio.open(self.outpath_cropped_corine)
         else:
-            raise ValueError("Dataset is not open.")
+            if self.verbose:
+                print('creating corine cropped image')
+            self.process_corine()
+            self.corine = rasterio.open(self.outpath_cropped_corine)
 
-    def generate_sample(self, num_points, aoi=None, to_sample_crs='EPSG:4326', rng=1414):
+    def even_sample(self, geometry, config):
+        """Generate an evenly spaced grid of centroids over a given geometry.
+
+        Args:
+            geometry (shapely.geometry.Polygon): Target polygon geometry.
+            cell_size (float): Size of each grid cell.
+
+        Returns:
+            geopandas.GeoDataFrame: Grid of centroid points within the geometry.
+        """
+        # Get bounding box
+        minx, miny, maxx, maxy = geometry.bounds
+
+        # Create a grid of points
+        x_coords = np.arange(minx, maxx, cell_size) + (cell_size / 2)
+        y_coords = np.arange(miny, maxy, cell_size) + (cell_size / 2)
+
+        # Generate centroid points
+        points = [Point(x, y) for x in x_coords for y in y_coords]
+
+        # Filter points inside the geometry
+        grid_points = [pt for pt in points if geometry.contains(pt)]
+
+        # Convert to GeoDataFrame
+        return gpd.GeoDataFrame(geometry=grid_points, crs="EPSG:4326")
+
+    def generate_sample(self, num_points, aoi=None, sample='random', to_sample_crs='EPSG:4326', rng=1414):
         if self.corine is None:
             print('Corine Dataset not loaded... loading now')
             self.load_corine()
         if aoi is None:
             aoi = self.border_austria
-            sample = aoi.sample_points(size=num_points, rng=rng)
+            if sample == 'random':
+                sample = aoi.sample_points(size=num_points, rng=rng)
+            else:
+                sample = ''
             points = gpd.GeoDataFrame(geometry=[p for p in sample[0].geoms], crs=aoi.crs)
         else:
             sample = aoi.sample_points(size=num_points, rng=rng)
+            if sample == 'random':
+                sample = aoi.sample_points(size=num_points, rng=rng)
+            else:
+                sample = ''
             # switched coords from wgs84 to european system
             points = gpd.GeoDataFrame(geometry=[shapely.Point(p.y, p.x) for p in sample[0].geoms], crs=aoi.crs)
 
@@ -170,22 +228,25 @@ class Sampler:
 
         if self.verbose:
             print(f'Generated {len(points_corine_filtered)} sample points in given AOI.')
-        points_corine_filtered.to_file('sample.gpkg', driver='GPKG')
+        points_corine_filtered.to_file('output/sample.gpkg', driver='GPKG')
 
         return
 
 
+
+config = Config(pixel_size=1.6, shape=(4, 1024, 1024))
 sampler = Sampler(sample_path='',
-                  corine_path=r"C:\Users\PC\Desktop\TU\Master\MasterThesis\data\corine\eea_r_3035_100_m_clc-2018-acc_p_2017-2018_v01_r00\eea_r_3035_100_m_clc-2018-acc_p_2017-2018_v01_r00\CLC2018ACC_V2018_20.tif",
-                  outpath_cropped_corine=r"C:\Users\PC\Desktop\TU\Master\MasterThesis\data\corine\corine_croppped.tif",
-                  verbose=True)
+                  corine_path="data/corine/CLC2018ACC_V2018_20.tif",
+                  outpath_cropped_corine="data/corine/corine_transformed.tif",
+                  verbose=True,
+                  config=config)
 
 sampler.load_corine()
 
 aoi_bbox = shapely.box(47.9290294921047, 16.073601498323555, 47.95607947801313, 16.133786632464748)
 aoi = gpd.GeoDataFrame(geometry=[aoi_bbox], crs='EPSG:4326')
 
-sampler.generate_sample(num_points=40000, aoi=None)
+#sampler.generate_sample(num_points=50, aoi=None, sample='even')
 
 pass
 # t.crop_full_corine()
